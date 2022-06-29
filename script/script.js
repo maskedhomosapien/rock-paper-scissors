23df1cfd9d54aa2d3044e294accea6c7b0d73304
Attuah Otabil <attuahotabilkay@gmail.com>
2022-06-29 10:54:24 +0000

    further breakdown of project

--- script/script.js
+++ script/script.js
@@ -9,7 +9,25 @@ keep score and report winner or looser at then end of the game
 use prompt to get the input from the user.
 
 BREAK DOWN THE PROJECT 
+input selection from players
+function computerPlay that return random selection from string array rock, paper and scissors
+
 funtion playRound
+return string that declares the winner
+
+rock beats scissors
+scissors beats paper
+paper beat rock
+
 funtion game and call playRound
 use loops to play five rounds
  */
+
+function playRound(playerSelection, computerSelection) {
+    // your code here!
+
+  }
+  
+  const playerSelection = "rock";
+  const computerSelection = computerPlay();
+  console.log(playRound(playerSelection, computerSelection));
