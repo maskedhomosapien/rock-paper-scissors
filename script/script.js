6f06c831d7e715fed2b0c420460e0863cce0f181
Attuah Otabil <attuahotabilkay@gmail.com>
2022-06-29 10:32:25 +0000

    understand and break down the project

---
+++ script/script.js
@@ -0,0 +1,15 @@
+/*
+UNDERSTAND THE PROJECT  
+A single round game of rock paper scissors with computer
+write function playRound that takes the parameter playerSelection and computerSelecion
+players selection input should be case-insensitive
+results of the function call should be returned 
+write a new function game and call the the playRound function
+keep score and report winner or looser at then end of the game
+use prompt to get the input from the user.
+
+BREAK DOWN THE PROJECT 
+funtion playRound
+funtion game and call playRound
+use loops to play five rounds
+ */
