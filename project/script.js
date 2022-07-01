38041e03a57d39116588baca0245cbbbb854f8b8
Attuah Otabil <attuahotabilkay@gmail.com>
2022-07-01 10:26:08 +0000

    breakdown the function game

--- project/script.js
+++ project/script.js
@@ -19,8 +19,24 @@ rock beats scissors
 scissors beats paper
 paper beat rock
 
-funtion game and call playRound
-use loops to play five rounds
+funtion game { 
+player points
+computer points
+
+{use loops to play five rounds
+takes input selection from user using prompt
+and input selection from computer using computerPlay 
+call the playRound to get outcome of each round
+equate playRound to result to keep outcome
+if result is a draw then dont add anything 
+if outcome is equal to this result then add one point to the player points
+else add one point to the computer points}
+ funtion to declare the winner 
+ if player is equal to computer points then its a tie
+ else if player is greater than the computer points then player wins
+ else computer wins
+}
+
  */
 let computerPlay = function() {     
     let computerOptions = ["rock", "paper", "scissors"];
